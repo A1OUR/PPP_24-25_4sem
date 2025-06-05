54f114645f1b4fd7362d8ce525468a5113ceeb8a
A1OUR <60852994+A1OUR@users.noreply.github.com>
2025-06-05 03:29:28 +0300

    коменты для понимания

--- 2lab/main.py
+++ 2lab/main.py
@@ -5,8 +5,8 @@ from app.api.websocket import router as ws_router
 import subprocess
 import time
 
-app = FastAPI(title="Encryption API")
+app = FastAPI(title="Encryption API") # создание класса фастапи
 
-app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
-app.include_router(encryption_router, prefix="/api/encryption", tags=["Encryption"])
-app.include_router(ws_router, tags=["WebSocket"])
+app.include_router(auth_router, prefix="/api/auth", tags=["Auth"]) #  подключаем роутеры с эндпоинтами из файла auth
+app.include_router(encryption_router, prefix="/api/encryption", tags=["Encryption"]) #  подключаем роутеры с эндпоинтами из файла ecrytption
+app.include_router(ws_router, tags=["WebSocket"]) #  подключаем роутеры с эндпоинтами из файла websocket
